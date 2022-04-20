227ebcfcd0469d972ce2b21eeadfc6ac31065faf
Al0ha0e <1919270783@qq.com>
2022-04-20 08:34:01 -0700

    linked list

--- src/libs/ds.c
+++ src/libs/ds.c
@@ -36,3 +36,108 @@ void *vector_get_item(vector *v, uint32 id)
         return NULL;
     return ((char *)v->buffer) + v->item_size * id;
 }
+
+static list_node *make_list_node(void *v)
+{
+    list_node *ret = malloc(sizeof(list_node));
+    ret->prev = ret->next = NULL;
+    ret->v = v;
+    return ret;
+}
+
+void list_insert_front(linked_list *list, list_node *node)
+{
+    if (!list->st)
+    {
+        list->st = list->en = node;
+        return;
+    }
+    node->next = list->st;
+    list->st->prev = node;
+    list->st = node;
+}
+
+void list_insert_back(linked_list *list, list_node *node)
+{
+    if (!list->en)
+    {
+        list->st = list->en = node;
+        return;
+    }
+    node->prev = list->en;
+    list->en->next = node;
+    list->en = node;
+}
+
+list_node *list_push_front(linked_list *list, void *v)
+{
+    list_node *ret = make_list_node(v);
+    list_insert_front(list, ret);
+    return ret;
+}
+
+list_node *list_push_back(linked_list *list, void *v)
+{
+    list_node *ret = make_list_node(v);
+    list_insert_back(list, ret);
+    return ret;
+}
+
+void list_remove(linked_list *list, list_node *node)
+{
+    if (node == list->st)
+    {
+        if (node == list->en)
+            list->st = list->en = NULL;
+        else
+        {
+            list->st = node->next;
+            list->st->prev = NULL;
+        }
+    }
+    else if (node == list->en)
+    {
+        list->en = node->prev;
+        list->en->next = NULL;
+    }
+    else
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+    node->prev = node->next = NULL;
+}
+
+void list_delete(linked_list *list, list_node *node)
+{
+    list_remove(list, node);
+    free(node);
+}
+
+void list_pop_front(linked_list *list)
+{
+    if (list->st)
+        list_delete(list, list->st);
+}
+
+void list_pop_back(linked_list *list)
+{
+    if (list->en)
+        list_delete(list, list->en);
+}
+
+void list_move_to_front(linked_list *list, list_node *node)
+{
+    if (node == list->st)
+        return;
+    list_remove(list, node);
+    list_insert_front(list, node);
+}
+
+void list_move_to_back(linked_list *list, list_node *node)
+{
+    if (node == list->en)
+        return;
+    list_remove(list, node);
+    list_insert_back(list, node);
+}
--- src/libs/ds.h
+++ src/libs/ds.h
@@ -16,4 +16,27 @@ void vector_push_back(vector *v, void *item);
 void *vector_extend(vector *v);
 void *vector_get_item(vector *v, unsigned int id);
 
+typedef struct list_node
+{
+    struct list_node *prev;
+    struct list_node *next;
+    void *v;
+} list_node;
+
+typedef struct linked_list
+{
+    list_node *st;
+    list_node *en;
+} linked_list;
+
+void list_insert_front(linked_list *list, list_node *node);
+void list_insert_back(linked_list *list, list_node *node);
+list_node *list_push_front(linked_list *list, void *v);
+list_node *list_push_back(linked_list *list, void *v);
+void list_remove(linked_list *list, list_node *node);
+void list_delete(linked_list *list, list_node *node);
+void list_pop_front(linked_list *list);
+void list_pop_back(linked_list *list);
+void list_move_to_front();
+void list_move_to_back();
 #endif
